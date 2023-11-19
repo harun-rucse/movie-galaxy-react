bf9bd5a1cc776707a13a728202719e4fa94218e7
harun-rucse <horrashid007@gmail.com>
2023-11-20 00:15:10 +0600

    Fixed loading skeleton mobile responsive

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -5,7 +5,7 @@ function ListItems({ children }) {
   return (
     <Container>
       <div className="w-full">
-        <div className="flex flex-wrap items-center w-full gap-x-3 md:gap-x-5 gap-y-8 overflow-hidden">
+        <div className="flex flex-wrap items-center w-full gap-x-2 md:gap-x-5 gap-y-8 overflow-hidden">
           {children}
         </div>
       </div>
--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -4,7 +4,10 @@ function Skeleton({ count }) {
   return (
     <div className="w-full h-full flex gap-4">
       {[...Array(count)].map((_, index) => (
-        <div key={index} className="w-[160px] md:w-[calc(20%-16px)]">
+        <div
+          key={index}
+          className="w-[160px] md:w-[calc(20%-16px)] flex-shrink-0"
+        >
           <div className="w-full aspect-[1/1.4] rounded-xl mb-3 skeleton" />
           <div className="flex flex-col space-y-2">
             <div className="w-full h-6 skeleton" />
